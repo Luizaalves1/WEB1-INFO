f6c1af5f15a3f30567001ef83ca5a292ef173f34
Luiza Andreza Alves de Sousa <landrezaal@gmail.com>
2025-12-09 13:49:16 -0300

    Atualização do roteador em main.jsx para incluir a rota da página Home

--- infocom-react/src/main.jsx
+++ infocom-react/src/main.jsx
@@ -3,13 +3,22 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home.jsx"; // importação da página Home
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+    ],
   },
 ]);
+
+// Resto do código permanece o mesmo
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
