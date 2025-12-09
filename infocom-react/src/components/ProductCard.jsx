204aacaee7ea4724e280f342a391cc202cfe1d29
Luiza Andreza Alves de Sousa <landrezaal@gmail.com>
2025-12-09 13:41:43 -0300

    Ajuste no componente ProductCard.jsx para incluir um elemento separador entre a imagem e o preço do produto e inverter a ordem do preço e do nome.

--- infocom-react/src/components/ProductCard.jsx
+++ infocom-react/src/components/ProductCard.jsx
@@ -2,13 +2,17 @@ import './ProductCard.css';
 
 function ProductCard({ product }) {
   return (
-    <div className="card">
+    // Dentro do return do componente ProductCard
+    <div className='card'>
       <img src={product.image} alt={product.title} />
+
+      <div className='separator'></div>
+
+      <div className='price'>R$ {product.price.toFixed(2).replace(".", ",")}</div>
+
       <h3>{product.title}</h3>
-      <div className="price">
-        R$ {product.price.toFixed(2).replace('.', ',')}
-      </div>
     </div>
+
   );
 }
 
