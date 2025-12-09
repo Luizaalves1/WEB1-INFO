efb218da081efc8ee91c6e71a3d3befcd7d87970
Luiza Andreza Alves de Sousa <landrezaal@gmail.com>
2025-12-09 16:53:33 -0300

    Alterações nos arquivos ProductCard.css e ProductsCard.jsx

--- infocom-react/src/components/ProductCard.jsx
+++ infocom-react/src/components/ProductCard.jsx
@@ -3,11 +3,17 @@ import './ProductList.css';
 
 function ProductList({ products }) {
   return (
-    <div className="grid">
-      {products.map((prod) => (
-        <ProductCard key={prod.id} product={prod} />
-      ))}
+    // Dentro do return do componente ProductCard
+    <div className='card'>
+      <img src={product.image} alt={product.title} />
+
+      <div className='separator'></div>
+
+      <div className='price'>R$ {product.price.toFixed(2).replace(".", ",")}</div>
+
+      <h3>{product.title}</h3>
     </div>
+
   );
 }
 
